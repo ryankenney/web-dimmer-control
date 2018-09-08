8d71d218b8eac9acf36d386dc6ac3c69e8263ef5
Ryan Kenney <ryan@ryankenney.info>
2018-09-08 15:39:33 -0700

    Switched to a modern version of jersey+moxy for json

---
+++ src/main/java/com/github/ryankenney/hitachi_web_control/rest/RestApplication.java
@@ -0,0 +1,24 @@
+package com.github.ryankenney.hitachi_web_control.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class RestApplication extends Application {
+	
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> resources = new HashSet<Class<?>>();
+
+		// register REST modules
+		resources.add(StatusService.class);
+		resources.add(ControlService.class);
+
+		// Manually adding MOXyJSONFeature
+		resources.add(org.glassfish.jersey.moxy.json.MoxyJsonFeature.class);
+
+		return resources;
+	}
+
+}
--- src/main/java/com/github/ryankenney/hitachi_web_control/rest/RestServer.java
+++ src/main/java/com/github/ryankenney/hitachi_web_control/rest/RestServer.java
@@ -5,28 +5,26 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class RestServer {
-//	public static void main(String[] args) throws Exception {
-//		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-//		context.setContextPath("/rest/");
-//
-//		Server jettyServer = new Server(8080);
-//		jettyServer.setHandler(context);
-//
-//		ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-//		jerseyServlet.setInitOrder(0);
-//
-//		jerseyServlet.setInitParameter("jersey.config.server.provider.classnames",
-//				GlobalExceptionMapper.class.getCanonicalName()+";"+
-//				ControlService.class.getCanonicalName()+";"+
-//				StatusService.class.getCanonicalName());
-//
-//		jerseyServlet.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true"); 
-//
-//		try {
-//			jettyServer.start();
-//			jettyServer.join();
-//		} finally {
-//			jettyServer.destroy();
-//		   }
-//    }
+	public static void main(String[] args) throws Exception {
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/rest/");
+
+		Server jettyServer = new Server(8080);
+		jettyServer.setHandler(context);
+
+		ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+		jerseyServlet.setInitOrder(0);
+
+		jerseyServlet.setInitParameter("javax.ws.rs.Application",
+				RestApplication.class.getName());
+
+		jerseyServlet.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true"); 
+
+		try {
+			jettyServer.start();
+			jettyServer.join();
+		} finally {
+			jettyServer.destroy();
+		   }
+    }
 }
